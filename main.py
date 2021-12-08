98d2344e988436972262f4d07e19a2faae6d78a8
sophieefoster <emil.lengyel@gmail.com>
2021-12-07 19:48:56 -0800

    changed the directory of main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ query = int(input("What base " + str(base1) + " number do you want to convert to
 
 #Converts the query into the base you are converting to.
 while query != 0:
-    step = query%base2
-    result = result + step*position
+    step = query % base2
+    result = result + step * position
     query = int(query/base2)
     position = position*10
+
 print(result)
 
 #Takes the output in base 6 and turns it into words in etalanu mosa nonuwu nomamono.
@@ -28,14 +29,15 @@ if base2 == 6:
     na = {10: "na"}
     mana = {100: "mana"}
     tekasu = {1000: "tekasu"}
-    ni = {1000000, "ni"}
-    pi = {1000000000, "pi"}
-    ji = {1000000000000, "ji"}
-    ki = {1000000000000000, "ki"}
-    si = {1000000000000000000, "si"}
+    ni = {1000000: "ni"}
+    pi = {1000000000: "pi"}
+    ji = {1000000000000: "ji"}
+    ki = {1000000000000000: "i"}
+    si = {1000000000000000000: "si"}
     for i in range(len(str(result))):
         value = int(str(result)[place]) * position
         print(value)
-        numbers.append(value)
+        numbers = numbers.append(value)
+        print(numbers)
         place = place - 1
         position = position * 10
